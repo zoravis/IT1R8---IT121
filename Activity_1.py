1cbb3857a44e237118ff8c04ba8d6cf95f038817
zoravis <celesiosavril@gmail.com>
2026-02-10 17:41:40 +0800

    #Added a comment Hello

--- Activity_1.py
+++ Activity_1.py
@@ -1,7 +1,7 @@
 name = "Avril Satu Celesios"
 age = 18
 height = "4'11"
-CurrentlyWorking = False
+CurrentlyWorking = False #helloa
 
 print("Your name is" , name , ", you are " , age , " years old, your height is" , height , ". are you currently working? " , CurrentlyWorking)
 
